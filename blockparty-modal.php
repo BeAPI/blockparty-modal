1ae34d2b93756db347665f1de527a540152a0f08
mricoul <mricoul@beapi.fr>
2026-02-18 14:11:03 +0100

    chore: updates plugin version to 1.0.1
    Bumps the plugin version across all relevant files.
    
    This version update includes a fix for the dialog element's margin style,
    setting it to 'auto' by default instead of '0'.

--- blockparty-modal.php
+++ blockparty-modal.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:       Blockparty Modal
  * Description:       Modal block for WordPress editor.
- * Version:           1.0.0
+ * Version:           1.0.1
  * Requires at least: 6.8
  * Requires PHP:      7.4
  * Author:            Be API Technical Team
@@ -19,7 +19,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
-define( 'BLOCKPARTY_MODAL_VERSION', '1.0.0' );
+define( 'BLOCKPARTY_MODAL_VERSION', '1.0.1' );
 define( 'BLOCKPARTY_MODAL_URL', plugin_dir_url( __FILE__ ) );
 define( 'BLOCKPARTY_MODAL_DIR', plugin_dir_path( __FILE__ ) );
 
