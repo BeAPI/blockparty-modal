4eff141763bcd54a5c93bd5b91e9b1951eb3f676
Milan Ricoul <mricoul@beapi.fr>
2026-02-19 11:53:38 +0000

    Release version 1.0.3

---
+++ build/blockparty-modal/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => 'f44e72d1e386f918284d');
