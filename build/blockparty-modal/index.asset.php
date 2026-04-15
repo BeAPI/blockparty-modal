1a6809407f5889df51d8bdc4f39861410fe00602
Marie Comet <mcomet@beapi.fr>
2026-04-15 08:06:42 +0000

    Release version 1.0.7

---
+++ build/blockparty-modal/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => 'e6200a6e47af43afd24a');
